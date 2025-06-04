aefba904ee18163298a2ad4322a48df6f54e336d
Claudiu Nedelcu <npclaudiu@users.noreply.github.com>
2025-06-05 01:45:42 +0300

    chore: add graphael client

--- cmd/graphael/main.go
+++ cmd/graphael/main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+
+	graphael "github.com/graphaelio/graphael-go-client"
 )
 
 func main() {
+	client := graphael.NewClient(graphael.ClientConfig{
+		BaseURL: nil, // Use default base URL
+		ID:      "your-client-id",
+		Secret:  "your-client-secret",
+	})
+
+	if client == nil {
+		fmt.Println("Failed to create Graphael client")
+		return
+	}
+
 	fmt.Println("Hello, world!")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module graphael-cli
+module github.com/graphaelio/graphael-cli
 
 go 1.24.0
+
+require github.com/graphaelio/graphael-go-client v0.0.0-20250604223227-698d457c54d4 // indirect
